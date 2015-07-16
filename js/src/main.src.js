ed34939e3c3ac237a649bd76d2cfe7f3db81dc5c
Jon Zielenkievicz <jzielenkievicz@nycgo.com>
2015-07-16 14:00:01 -0400

    move GA code to min.js file

--- js/src/main.src.js
+++ js/src/main.src.js
@@ -52,4 +52,19 @@ $(function() {
     $('#mc-embedded-subscribe-form').on('keyup', '#mce-EMAIL', function() {
         checkInput(this.value);
     });
+
+    // check for dev environment
+    var str = window.location.host;
+    var patt = /(\.dev$|\.zielenkievicz\.com$)/g;
+    var match = patt.test(str);
+
+    if (!match) {
+        // Google Analytics
+        (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+        function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+        e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+        e.src='https://www.google-analytics.com/analytics.js';
+        r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+        ga('create','UA-64909606-1','auto');ga('send','pageview');
+    }
 });
