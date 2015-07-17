c3f95a00d83c6cef559e19beafa729fe0581ce1e
Jon Zielenkievicz <jonzielen@gmail.com>
2015-07-16 21:37:05 -0400

    remove test code

--- js/src/plugins.src.js
+++ js/src/plugins.src.js
@@ -26,7 +26,6 @@ var patt = /(\.dev$|\.zielenkievicz\.com$)/g;
 var match = patt.test(str);
 
 if (!match) {
-    console.log('here');
     (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
     function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
     e=o.createElement(i);r=o.getElementsByTagName(i)[0];
