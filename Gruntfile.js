2f81732eb9e94296a4af16f7c89ba82879033aee
Jon Zielenkievicz <jzielenkievicz@nycgo.com>
2015-07-22 10:15:56 -0400

    refresh init

---
+++ Gruntfile.js
@@ -0,0 +1,70 @@
+module.exports = function (grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    compass: {
+      dist: {
+        options: {
+          raw: 'require "compass/import-once/activate"\n',
+          sassDir:'sass',
+          cssDir:'css',
+          cacheDir:'.sass-cache',
+          httpPath:'/',
+          imagesDir:'img',
+          javascriptsDir:'js',
+          outputStyle:'compressed'
+        }
+      }
+    },
+    uglify: {
+      options: {
+        mangle:true,
+        compress:true
+      },
+      all: {
+        files: [{
+          expand: true,
+          cwd: 'js/src',
+          src: ['*.src.js', '!*.min.js'],
+          dest: 'js/min',
+          ext: '.min.js'
+        }],
+      },
+    },
+    watch: {
+      scripts: {
+        files:[
+          'js/src/*.src.js'
+        ],
+        tasks:['uglify'],
+        options: {
+          spawn:false,
+          livereload:true
+        },
+      },
+      css: {
+        files:[
+          'sass/*.scss'
+        ],
+        tasks:['compass'],
+        options: {
+          spawn:false,
+          livereload:true
+        }
+      },
+      html: {
+        files:['*.html'],
+        options: {
+          spawn:false,
+          livereload:true
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('default', ['compass', 'uglify']);
+};
