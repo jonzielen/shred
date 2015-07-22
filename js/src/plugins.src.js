6043b452ec68c26071c8fe070955050c8b6a5efc
Jon Zielenkievicz <jzielenkievicz@nycgo.com>
2015-07-22 14:14:56 -0400

    remove unused styles, clean up js/gitignore

--- js/src/plugins.src.js
+++ js/src/plugins.src.js
@@ -22,10 +22,10 @@
 }());
 // Google Analytics
 var str = window.location.host;
-var patt = /(\.dev$|\.zielenkievicz\.com$)/g;
+var patt = /(shredvideo\.com$)/g;
 var match = patt.test(str);
 
-if (!match) {
+if (match) {
     (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
     function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
     e=o.createElement(i);r=o.getElementsByTagName(i)[0];
